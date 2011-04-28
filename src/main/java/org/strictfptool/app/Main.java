837543f88e7fa46f67f6eb3e4f73a06798eaf456
Martin Pärtel <martin.partel@gmail.com>
2011-04-28 17:33:57 +0300

    Main: half-finished, currently runs CallGraphBuilder.

--- src/main/java/org/strictfptool/app/Main.java
+++ src/main/java/org/strictfptool/app/Main.java
@@ -1,16 +1,18 @@
 package org.strictfptool.app;
 
-import java.util.List;
-
+import org.strictfptool.analysis.results.BasicCallGraphAnalysis;
+import org.strictfptool.callgraph.CallGraphBuilder;
+import org.strictfptool.ignoreset.EmptyIgnoreSet;
 import org.strictfptool.loaders.ClassFileLoader;
 import org.strictfptool.loaders.ClassPathClassFileLoader;
 import org.strictfptool.loaders.DefaultClassFileLoader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Settings settings = readArgs(args);
         ClassFileLoader classFileLoader = makeClassFileLoader(settings);
-        processTargets(settings.targets, classFileLoader);
+        BasicCallGraphAnalysis basicResult = buildCallgraph(settings, classFileLoader);
+        System.out.println("TODO...");
     }
 
     private static Settings readArgs(String[] args) {
@@ -39,9 +41,18 @@ public class Main {
         }
     }
     
-    private static void processTargets(List<String> targets, ClassFileLoader classFileLoader) {
-        for (String target : targets) {
-            //TODO
+    private static BasicCallGraphAnalysis buildCallgraph(Settings settings, ClassFileLoader classFileLoader) throws Exception {
+        CallGraphBuilder builder = new CallGraphBuilder(classFileLoader, EmptyIgnoreSet.getInstance());
+        builder.setDebugTrace(settings.trace);
+        
+        for (String target : settings.targets) {
+            if (target.contains("::")) {
+                String[] parts = target.split("::");
+                builder.addRootMethod(parts[0], parts[1]);
+            } else {
+                builder.addRootClass(target.replace('.', '/'));
+            }
         }
+        return builder.getResult();
     }
 }
